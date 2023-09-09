f023d182d92ac150a06be956113b18947ca752cd
osama <zoror861@gmail.com>
2023-09-09 03:14:00 -0700

    f

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return 'None'
-    elif idx > len(my_list):
+    elif idx > len(my_list)-1:
         return 'None'
     for i in range (0,len(my_list)):
         if my_list[idx]==my_list[i]:
