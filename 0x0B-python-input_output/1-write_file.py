e8a2e36f8b8b1a813764bab4373e2fc22180ac06
osama <zoror861@gmail.com>
2023-10-10 16:40:10 -0700

    d

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""good ksdgn sdkgnksd s sdns gn"""
+
+
+def write_file(filename="", text=""):
+    """good foefjofmw fwfn wf
+
+    Args:
+        skdn:kdni sgsegesgneg kg
+        ekgn:iegesbg sekgnseibges
+
+    Raises: sdknfdskbs dknsdk kdsgsdkbg sdbg
+
+    """
+    with open(filename,'w',encoding='UTF8')as file :
+        return  file.write(text)
