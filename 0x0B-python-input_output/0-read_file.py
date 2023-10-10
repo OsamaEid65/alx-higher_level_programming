8899a8d320b8f7abf108df0c400dff4c1023f92e
osama <zoror861@gmail.com>
2023-10-10 15:46:56 -0700

    d

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""this fucaomr rkgnrgkrng kg k dgkndkdg kd g"""
 def read_file(filename=""):
+    """gioe sgnsdkgns dgkdsfngksd gksdngkn"""
     with open(filename, 'rt') as file:
+        """kgnkrsdng kdng mdg nk gkdrn"""
         print(file.read())
