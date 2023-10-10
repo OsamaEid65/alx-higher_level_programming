a49dcd1f5f797ecedc8461ddb99b267fbbc1d9b8
osama <zoror861@gmail.com>
2023-10-10 16:32:18 -0700

    d

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,14 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """;kdfmbksdmb  dvdv  dvdvv"""
+
+
 def read_file(filename=""):
     """dvmld f f fdml vlv
-
     Args:
         sdjg:ajfaf skrngsr gsekngse kgnse
         sfnskfn:skegnskgn ksv sdfk dgdg
     Raises: sdjg sg eg sdgn  ekng
     """
-    with open(filename, 'rt',encoding="UTF8") as file:
+    with open(filename, 'rt', encoding="UTF8") as file:
         """dlmv lf lm,dvldvm dg """
         print(file.read())
