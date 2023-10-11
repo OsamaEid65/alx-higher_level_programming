553e3903349296384c528a9091135b5fa55b13d0
osama <zoror861@gmail.com>
2023-10-10 17:36:39 -0700

    d

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""jsns ksnfsmd vsdklnbs b"""
+
+
+def save_to_json_file(my_obj, filename):
+    """kengisg skefnskel gksegse gseg seklgn"""
+    import json
+    """skdnvsd gksdng sgksng segn
+
+    Args:
+    dfjbisdhg:sdgjsng
+    skdgnsgn:sgnisrgsg
+
+    Raises:skdigsngspg ksegnse gskegnsekgnesg ksengkseg seignske;gn
+
+    """
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
+        return f
