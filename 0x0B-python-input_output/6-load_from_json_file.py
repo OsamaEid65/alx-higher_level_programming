667bb845448931a09abccdde41999cff2a9db7bd
osama <zoror861@gmail.com>
2023-10-10 17:48:38 -0700

    f

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""ksldnfksdg sdkvnsd gvskngsd kgns"""
+
+
+def load_from_json_file(filename):
+    """jsnbgsde fsd fklsnsmd. sekl s.dv sdkv sdk sd"""
+    with open(filename, 'r') as f:
+        """sjgsngsk;ngs gsgn
+        Args:
+        sdgnsgn:sjegsrg
+        sdgnjsgn:sdbnsgn
+
+        Raises:sikgsngsngske gsekfse ,segnske gksein
+        """
+        import json
+        json_data = json.load(f)
+        return json_data
