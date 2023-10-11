e841a7cdc19fa001adead31c4446c0496aab2952
osama <zoror861@gmail.com>
2023-10-10 17:09:28 -0700

    f

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" goo kbfkef eg e geknfs cfkdn"""
+import json
+"""ksdnsd skegs dvbsvsekgnse fekf"""
+def to_json_string(my_obj):
+    """jsdbvsdjgkls ksdv sdbvs ggsje
+
+    Args:
+        ahefsief:keskdng  rgergherh erhrhrh
+        akdvhdsbv:aksdvbsdgsdb sg seg se gs r
+
+    Raises:ishgiegkesg ,sdns bkrnbsk gegbewg sgn
+    """
+    json_string=json.dumps(my_obj)
+    return json_string
