5c10882f618d85414c3c93db8b00522e69553f9b
osama <zoror861@gmail.com>
2023-10-10 17:08:30 -0700

    d

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""osejgeosg sekgner gkrgnker giweng"""
+
+
+def append_write(filename="", text=""):
+    """sknsgnse gsebg
+
+    Args:
+        kdnvdksn: aknfsefn
+        sgnsgn:sekgnskgnskgns
+    Raises:skngks;dng skgnsg skrngvsdk vkds gsng
+
+    """
+    with open(filename, 'a', encoding="UTF8")as f:
+        return f.write(text)
