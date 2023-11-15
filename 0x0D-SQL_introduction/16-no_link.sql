b2de9de838381dba2daffda6390613fc0637fc85
osama <zoror861@gmail.com>
2023-11-14 18:05:42 -0800

    d

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,3 @@
 --  lists all records of the table second_table of the database hbtn_0c_0
-select score , name  FROM second_table ORDER BY score DESC ;
+select score , name  FROM second_table WHERE
+    `name` IS NOT NULL ORDER BY score DESC ;
