38b97cb29b1843d3941e2bc99025d134f53503fc
osama <zoror861@gmail.com>
2023-11-14 18:10:06 -0800

    d

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,6 @@
---  lists all records of the table second_table of the database hbtn_0c_0
-select score , name  FROM second_table WHERE
-    `name` IS NOT NULL ORDER BY score DESC ;
+-- Lists all records of the table second_table having a name value in my MySQL server.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
